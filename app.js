7f41f964c5f4596ca8b81e88ff823d0c662c639a
Kaung Myat <kaung8229@gmail.com>
2023-07-24 19:33:06 +0630

    updated multiply section 24-7-2023

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 // GET UI
 // input ui
 let darkbtn = document.querySelector('.darkmode-btn');
+let togglesecbtn = document.querySelector('.toggle-section');
 
 let inputcontainer = document.querySelector('.input-container');
 let testingcontainer = document.querySelector('.testing-container');
+let titlesec = document.querySelector('.title-sec');
 
+let forpluscon = document.querySelector('.forplus');
+let formultiplycon = document.querySelector('.formultiply');
+
+// for plsu input
 let levelinput = document.getElementById('level');
 let limitinput = document.getElementById('limit');
 let digsinput = document.getElementById('digs');
@@ -13,8 +19,21 @@ let minusinput = document.getElementById('minus');
 let leverr = document.querySelector('.leverr');
 let leverr2 = document.querySelector('.leverr2');
 let digserr = document.querySelector('.digserr');
+// for plsu input
 
-let startbtn = document.querySelector('.submit-btn');
+
+// for multiply input
+let multilevelinput = document.getElementById('multilevel');
+let multidigsinput = document.getElementById('multidigs');
+
+let mleverr = document.querySelector('.multileverr');
+let mleverr2 = document.querySelector('.multileverr2');
+let mdigserr = document.querySelector('.multidigserr');
+// for multiply input
+
+
+let startplusbtn = document.querySelector('.plus');
+let startmultibtn = document.querySelector('.multi');
 // input ui
 
 
@@ -25,12 +44,22 @@ let teslevels = document.getElementById('levels');
 
 let testimes = document.getElementById('times');
 
-let quescontainer = document.querySelector('.ques-container');
+let plusquescont = document.querySelector('.plus-ques');
+let multiquescont = document.querySelector('.multi-ques');
 
+let plusques = document.querySelector('.plusque');
 let ansinput = document.getElementById('ans');
 
-let backbtn = document.querySelector('.back-btn');
-let nextbtn = document.querySelector('.ans-btn');
+let pbackbtn = document.querySelector('.pback-btn');
+let pnextbtn = document.querySelector('.pans-btn');
+
+
+let mtnumui = document.querySelector('.mtnum');
+let multiplierui = document.querySelector('.multiplier');
+let multiansinput = document.getElementById('multians');
+
+let mbackbtn = document.querySelector('.mback-btn');
+let mnextbtn = document.querySelector('.mans-btn');
 // testing ui
 
 
@@ -39,7 +68,9 @@ let scorebox = document.querySelector('.blur');
 
 let awardui = document.querySelector('.award');
 
-let scoreui = document.getElementById('score');
+// let scoreui = document.getElementById('score');
+let inclist = document.querySelector('.inc-list');
+
 let correctansui = document.getElementById('correctans');
 let totalqesui = document.getElementById('totalqes');
 let curtimeui = document.getElementById('curtime');
@@ -57,6 +88,7 @@ let historylist = document.querySelector('.history-list');
 var level = 5;
 var digits = 1;
 var limit = 10;
+var stepplus = 0;
 
 var curlevel = 1;
 var curidx = 0;
@@ -68,7 +100,7 @@ var coranses = [];
 var uncorlevels = [];
 
 var correctques = 0;
-var score = 5;
+// var score = 5;
 
 
 var mivals = [];
@@ -94,6 +126,48 @@ darkbtn.addEventListener('click',function(){
 
 })
 
+var sectionfix = JSON.parse(localStorage.getItem('section'));
+
+if(sectionfix == "multiply"){
+    forpluscon.classList.toggle("d-none");
+    formultiplycon.classList.toggle("d-none");
+
+    plusquescont.classList.toggle("d-none");
+    multiquescont.classList.toggle("d-none");
+
+    togglesecbtn.classList.toggle("active");
+
+    localStorage.setItem('section',JSON.stringify("addition"));
+}
+
+togglesecbtn.addEventListener('click',function(){
+    forpluscon.classList.toggle("d-none");
+    formultiplycon.classList.toggle("d-none");
+
+    plusquescont.classList.toggle("d-none");
+    multiquescont.classList.toggle("d-none");
+
+    if(!forpluscon.classList.contains("d-none")){
+        levelinput.focus();
+    }
+
+    if(!formultiplycon.classList.contains("d-none")){
+        multilevelinput.focus();
+    }
+    
+    this.classList.toggle("active");
+
+    if(this.classList.contains("active")){
+        this.innerText = "Addition";
+        titlesec.innerText = "( multiply )";
+        testingcontainer.classList.add("multipling");
+    }else{
+        this.innerText = "Multiply";
+        titlesec.innerText = "( addition )";
+        testingcontainer.classList.remove("multipling");
+    }
+})
+
 
 levelinput.focus();
 levelinput.addEventListener('keyup',function(){
@@ -105,7 +179,17 @@ digsinput.addEventListener('keyup',function(){
     digserr.classList.add('d-none');
 })
 
-startbtn.addEventListener('click',function(){
+multilevelinput.addEventListener('keyup',function(){
+    mleverr.classList.add('d-none');
+    mleverr2.classList.add('d-none');
+})
+
+multidigsinput.addEventListener('keyup',function(){
+    mdigserr.classList.add('d-none')
+})
+
+
+startplusbtn.addEventListener('click',function(){
     let levelin = levelinput.value;
     let digsin = digsinput.value;
     let limitin = limitinput.value;
@@ -117,7 +201,8 @@ startbtn.addEventListener('click',function(){
             level = +levelin;
             digits = +digsin;
             limit = +limitin;
-            starttrain();
+            starttrainplus();
+            darkbtn.classList.add("d-none");
         }else{
             leverr2.classList.remove('d-none');
         }
@@ -126,17 +211,38 @@ startbtn.addEventListener('click',function(){
         digsin == '' ? digserr.classList.remove('d-none') : digserr.classList.add('d-none');
     }
 
+    localStorage.setItem('section',JSON.stringify("addition"));
+
 })
 
+startmultibtn.addEventListener('click',function(){
+    let levelin = +multilevelinput.value;
+    let digsin = +multidigsinput.value;
+
+    if(levelin != '' && digsin != ''){
+        if(levelin >= level){
+            level = +levelin;
+            digits = +digsin;
+            starttrainmulti();
+            darkbtn.classList.add("d-none");
+        }else{
+            mleverr2.classList.remove('d-none');
+        }
+    }else{
+        levelin == '' ? mleverr.classList.remove('d-none') : mleverr.classList.add('d-none');
+        digsin == '' ? mdigserr.classList.remove('d-none') : mdigserr.classList.add('d-none');
+    }
+
+    localStorage.setItem('section',JSON.stringify("multiply"));
+})
 
 startagainbtn.addEventListener('click',function(){
     window.location.reload();
 })
 
 
-function starttrain(){
+function starttrainplus(){
     // console.log(levs);
-
     inputcontainer.classList.add('d-none');
     testingcontainer.classList.remove('d-none');
 
@@ -146,10 +252,28 @@ function starttrain(){
     tesbylevui.innerText = curlevel;
     teslevels.innerText = level;
 
+    // console.log(level/3);
+    var timebylev = Math.floor(level/3);
+    stepplus = (3 / timebylev).toFixed(2);
 
-    newquesfun();
+    // console.log(+stepplus);
+
+    newquesplusfun();
     timeinter = setInterval(timefun,1000);
+}
 
+function starttrainmulti(){
+    inputcontainer.classList.add('d-none');
+    testingcontainer.classList.remove('d-none');
+
+    multiansinput.focus();
+
+    teslevui.innerText = curlevel;
+    tesbylevui.innerText = curlevel;
+    teslevels.innerText = level;
+
+    newquesmultifun();
+    timeinter = setInterval(timefun,1000);
 }
 
 // time section
@@ -178,9 +302,10 @@ function timefun(){
 // time section
 
 
-var step = 7;
+var step = 6;
+var countre = 0;
 
-nextbtn.addEventListener('click',function(e){
+pnextbtn.addEventListener('click',function(e){
     e.preventDefault();
 
     if(ansinput.value.trim() != ''){
@@ -197,11 +322,17 @@ nextbtn.addEventListener('click',function(e){
                 // console.log("no data");
                 anses.push(ansinput.value);
                 ansinput.value = '';
-                scorefun();
+                scorefun(anses[curidx],coranses[curidx]);
                 curidx += 1;
-                // console.log(anses);
-                step += 1;
-                newquesfun();
+                step += +stepplus;
+                if(step == 9){
+                    step += 3;
+                }else{
+                    if(step == 15){
+                        step += 3;
+                    }
+                }
+                newquesplusfun();
             }else{
                 // console.log("data");
                 // console.log(anses[curidx]);
@@ -222,15 +353,16 @@ nextbtn.addEventListener('click',function(e){
 
             if(curlevel == level){
                 this.innerText = "Submit";
+                this.classList.add("active");
             }
 
-            htmlques(ques);
+            htmlquesplus(ques);
         }else{
             clearInterval(timeinter);
             this.parentElement.parentElement.remove();
             scorebox.classList.remove('d-none');
             anses.push(ansinput.value);
-            scorefun();
+            scorefun(anses[curidx],coranses[curidx]);
         }
         
         // console.log(ques);
@@ -240,7 +372,7 @@ nextbtn.addEventListener('click',function(e){
     }
 })
 
-backbtn.addEventListener('click',function(e){
+pbackbtn.addEventListener('click',function(e){
     e.preventDefault();
 
     // console.log(anses);
@@ -258,26 +390,90 @@ backbtn.addEventListener('click',function(e){
     ansinput.value = anses[curidx];
     ansinput.focus();
 
-    htmlques(ques);
+    htmlquesplus(ques);
 })
 
+mnextbtn.addEventListener('click',function(e){
+    e.preventDefault();
 
-function newquesfun(){
+    if(multiansinput.value.trim() != ''){
+        this.parentElement.classList.add("backactive");
+
+        if(curlevel < level){
+
+            if(anses.indexOf(anses[curidx]) == -1){
+                anses.push(multiansinput.value);
+                multiansinput.value = '';
+                scorefun(anses[curidx],coranses[curidx]);
+                curidx += 1;
+                newquesmultifun();
+            }else{
+                anses[curidx] = multiansinput.value;
+                curidx += 1;
+                if(anses.indexOf(anses[curidx]) != -1){
+                    multiansinput.value = anses[curidx];
+                }else{
+                    multiansinput.value = '';
+                }
+            }
+
+            multiansinput.focus();
+
+            curlevel++;
+
+            if(curlevel == level){
+                this.innerText = "Submit";
+                this.classList.add("active");
+            }
+
+            htmlquesmulti(ques);
+        }else{
+            clearInterval(timeinter);
+            this.parentElement.parentElement.remove();
+            scorebox.classList.remove('d-none');
+            anses.push(multiansinput.value);
+            scorefun(anses[curidx],coranses[curidx]);
+        }
+
+    }else{
+        multiansinput.focus();
+    }
+})
+
+mbackbtn.addEventListener('click',function(e){
+    e.preventDefault();
+
+    curlevel -= 1;
+    curidx -= 1;
+
+    if(curidx == 0){
+        this.parentElement.classList.remove('backactive');
+    }
+
+    multiansinput.value = anses[curidx];
+    multiansinput.focus();
+
+    htmlquesmulti(ques);
+})
+
+function newquesplusfun(){
     var ranque = [];
     var corans = 0;
-    var countre = 1;
+    countre = 0;
 
     var l=0;
-    while(l < step){
+    // console.log(Math.round(step));
+    // console.log(step);
+    while(l <= step){
         var rannum = randomnumfun(digits);
         // console.log(rannum);
 
         if(minusinput.checked){
-            console.log(step);
-            if(step == 5){
+            // console.log(step);
+            if(step == 6){
                 rannum *= Math.sign(rannum) == -1 ? -1 : 1;
             }else{
-                if(l == (step-1) || l == (step-2)){
+                if(l >= (step-1) || l >= (step-2)){
                     // console.log(minusvalues);
                     if(minusvalues.length == 0){
                         rannum *= Math.sign(rannum) == 1 ? -1 : 1;
@@ -293,36 +489,72 @@ function newquesfun(){
         // console.log("+++++ "+countre);
         countre += 1;
 
-        if(countre > limit){
+        if(countre >= limit){
             l += 30;
         }else{
-            l += 2;
+            l += 3;
         }
     }
+    // console.log(countre);
 
     ques.push(ranque);
     // console.log(ques);
     coranses.push(corans);
     // console.log(coranses)
 
-    htmlques(ques);
+    htmlquesplus(ques);
+}
+
+function newquesmultifun(){
+    let ranque = {
+        mtnum: randomnumfun(digits),
+        multiplier: randomnumfun(1)
+    };
+    // console.log(ranque.mtnum * ranque.multiplier);
+    console.log(ranque.multiplier);
+    console.log(ranque.mtnum);
+    if(ranque.mtnum == 1){
+        ranque.mtnum = ranque.mtnum + randomnumfun(digits);
+    }
+
+    if(ranque.multiplier == 1){
+        ranque.multiplier = ranque.multiplier + randomnumfun(1);
+    }
+
+    let corans = ranque.mtnum * ranque.multiplier;
+
+    ques.push(ranque);
+    // console.log(ques);
+    coranses.push(corans);
+
+    htmlquesmulti(ques);
 }
 
 
-function htmlques(que){
-    quescontainer.innerHTML = '';
+function htmlquesplus(que){
+    plusques.innerHTML = '';
     
     // console.log(ques);
     let curque = que[curidx];
     // console.log(curque);
 
     for(var q=0; q < curque.length; q++){
-
         var newli = document.createElement('li');
         newli.innerText = curque[q];
-
-        quescontainer.append(newli);
+        plusques.append(newli);
     }
+    teslevui.innerText = curlevel;
+    tesbylevui.innerText = curlevel;
+}
+
+function htmlquesmulti(que){
+    mtnumui.innerText = '';
+    multiplierui.innerText = '';
+
+    let curque = que[curidx];
+
+    mtnumui.innerText = curque.mtnum;
+    multiplierui.innerText = curque.multiplier;
     teslevui.innerText = curlevel;
     tesbylevui.innerText = curlevel;
 }
@@ -387,7 +619,9 @@ function randomnumfun(digits){
 }
 
 
-function scorefun(){
+function scorefun(userans,corans){
+    // console.log(userans);
+    // console.log(corans);
     // console.log(anses);
     // console.log(coranses);
     // console.log(ques);
@@ -397,12 +631,11 @@ function scorefun(){
 
     historylist.innerHTML = '';
 
-
-    var ansuser = +anses[curidx];
-    var anscor = coranses[curidx];
-    var quecor = ques[curidx];
-    // console.log(quecor.length);
-    score += (+quecor.length * 2);
+    var ansuser = +userans;
+    var anscor = +corans;
+    // var quecur = curque;
+    // console.log(quecur.length);
+    // score += (+quecur.length * 2);
 
     if(ansuser == anscor){
         correctques++;
@@ -413,11 +646,21 @@ function scorefun(){
 
     uncorlevels.forEach(uncorlevel=>{
         // console.log(coranses[uncorlevel]);
+        // console.log(anses);
         // console.log(anses[uncorlevel]);
 
         var unans = anses[uncorlevel];
         var coans = coranses[uncorlevel];
-        var unqus = ques[uncorlevel].join(' ');
+        var unqus = '';
+        // console.log(multiquescont.classList.contains('d-none'));
+
+        if(multiquescont.classList.contains('d-none')){
+            unqus = ques[uncorlevel].join(' ');
+        }else{
+            unqus = `${ques[uncorlevel].mtnum} x ${ques[uncorlevel].multiplier}`;
+        }
+
+        
         // console.log(unqus);
 
         // <li class="his-item">
@@ -443,11 +686,12 @@ function scorefun(){
     // console.log(correctques);
     // console.log(level/3);
 
-    awardui.innerText = "HELLO";
+    // awardui.innerText = "HELLO";
 
     if(correctques == level){
         awardui.innerText = "Perfect";
         awardui.className = "award perfect";
+        inclist.classList.add('d-none');
     }else if(correctques > (level/1.5)){
         awardui.innerText = "Great";
         awardui.className = "award great";
@@ -466,7 +710,7 @@ function scorefun(){
     let testimes = document.getElementById('times');
     var curtime = testimes.innerText;
 
-    scoreui.innerText = score;
+    // scoreui.innerText = score;
     curtimeui.innerText = curtime;
     correctansui.innerText = correctques;
     totalqesui.innerText = level;
